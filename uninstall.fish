18a746b742326cc1c99d183ef061ca5305f9e3b6
Justin Bennett <zephraph@gmail.com>
2019-11-13 23:55:46 -0500

    Add uninstall script

--- uninstall.fish
+++ uninstall.fish
@@ -2,3 +2,34 @@
 #
 # You can use this file to do custom cleanup when the package is uninstalled.
 # You can use the variable $path to access the package path.
+
+# Remove aliases
+abbr -e y
+abbr -e ya
+abbr -e yad
+abbr -e yap
+abbr -e yb
+abbr -e ycc
+abbr -e yd
+abbr -e yga
+abbr -e ygls
+abbr -e ygrm
+abbr -e ygu
+abbr -e yh
+abbr -e yi
+abbr -e yin
+abbr -e yls
+abbr -e yout
+abbr -e yp
+abbr -e yrm
+abbr -e yrun
+abbr -e ys
+abbr -e yst
+abbr -e yt
+abbr -e ytc
+abbr -e yuc
+abbr -e yui
+abbr -e yup
+abbr -e yv
+abbr -e yw
+abbr -e yws
